8fddc91464d9dff6398b2ccfa07f05fb85b6d004
satsawat <satsawatrodketkul@gmail.com>
2023-11-15 22:54:28 +0700

    initial project

---
+++ src/test/java/com/example/crudjavaspring/CrudJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
